1f711c1c180883bd0e075f4c3d998dda28b19f35
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-10-14 08:48:01 +0200

    Aufgabe 01

---
+++ JavaScript/01 Variablen/script.js
@@ -0,0 +1,10 @@
+console.log("Hallo Welt!");
+console.log("tolle Sache")
+
+// Variablen
+let a = 5;
+let b = 10;
+let c = a + b;
+console.log("A hat den wert " + a);
+console.log("a + b hat den wert " + c)
+
