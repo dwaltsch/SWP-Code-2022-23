210a1c83f7d0b8121715ff2655536b612ab0e8cd
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-10-14 09:28:18 +0200

    Aufgabe 03
    lommas

---
+++ JavaScript/04 Loops/script.js
@@ -0,0 +1,15 @@
+console.log("Hallo Welt!");
+
+// 7 45
+
+for (let i = 7; i <= 45; i++) {
+    console.log(i);
+}
+
+// 9 99
+for (let i = 9; i <= 99; i++) {
+    console.log(i);
+}
+for (let i = 10; i >= 0 ; i--) {
+    console.log(i)
+}
