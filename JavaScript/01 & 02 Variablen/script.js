558a39363f7bcdad2579e2fc65829c6bbfdb647c
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-10-14 08:58:19 +0200

    Aufgabe 02

---
+++ JavaScript/01 & 02 Variablen/script.js
@@ -0,0 +1,28 @@
+console.log("Hallo Welt!");
+console.log("tolle Sache")
+
+// Variablen
+let a = 5;
+let b = 10;
+let w = a + b;
+
+console.log("A hat den wert " + a);
+
+
+// Kommazahlen
+let c = 7.4;
+let d = 3.2;
+let e = c + d;
+console.log(e)
+
+let name = "Max";
+console.log("Hallo " + name);
+
+let x = 17.4;
+let y = 3.9;
+
+console.log("The result of " + x + " /  " + y + " is " + (x / y));
+
+let vorname = "Max";
+let nachname = "Mustermann";
+console.log("Hallo ich bin " + vorname + " " + nachname);
