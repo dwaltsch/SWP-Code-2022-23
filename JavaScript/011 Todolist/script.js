325a6a098389f46a45c6d3d8765f8a2cc6662234
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-10-21 09:29:49 +0200

    Todolist 1

---
+++ JavaScript/011 Todolist/script.js
@@ -0,0 +1,20 @@
+const tasks = ["tun", "nichtstun" , "machen"];
+
+printTaskList();
+
+function getHTMLTasks() {
+    let html = "";
+    tasks.forEach((element) => {
+        html += `<li>${element}</li>`;
+    });
+    return html;
+}
+function printTaskList() {
+    document.getElementById("tasklist").innerHTML = getHTMLTasks();
+}
+
+document.getElementById("addTask").addEventListener("click", function() {
+    let newTask = document.getElementById("txtNewTask").value;
+    tasks.push(newTask);
+    printTaskList();
+});
