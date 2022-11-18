f5b10264ce111d7afd70bbb79f5d0601a6d5e8c9
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-11-18 09:17:29 +0100

    Waehrungsrechner 2

--- JavaScript/012 Aufgaben/Waehrungsrechner/script.js
+++ JavaScript/012 Aufgaben/Waehrungsrechner/script.js
@@ -1,8 +1,23 @@
 function convert() {
     let amount = document.getElementById("txtEuro").value;
-    let converted = amount * 0.87;
-    let html = "<p>" + converted + " GBP</p>";
-    document.getElementById("value").innerHTML= html;
+    let currency = document.getElementById("currency").value;
+        let converted = calculateamount(amount,currency)
+        document.getElementById("value").innerHTML= converted;
+
+}
+function calculateamount(amount,currency){
+    if(currency == "CHF"){
+        var exchangerate = 1.01;
+    }
+    if(currency == "USD"){
+        var exchangerate = 1.04;
+    }
+    if(currency == "GBP"){
+        var exchangerate = 0.87;
+    }
+    let converted =  amount * exchangerate;
+    let html = "<p>" + converted + " " + currency+ "</p>";
+    return html
 }
 document.getElementById("convert").addEventListener("click", function () {
     convert();
