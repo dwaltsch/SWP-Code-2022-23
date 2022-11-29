063239ff953d12370f2dda2cee19dc25687eb671
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2022-11-29 12:18:46 +0100

    06 Rechner

--- JSHTML/06 Rechner/script.js
+++ JSHTML/06 Rechner/script.js
@@ -1,7 +1,11 @@
 function buttonClicked(){
-    console.log("clicked")
     let num1 = document.getElementById("number1").value;
     let num2 = document.getElementById("number2").value;
-    let result = parseFloat(num1) + parseFloat(num2);
-    console.log()
+    let resultadd = parseFloat(num1) + parseFloat(num2);
+    let resultsub = parseFloat(num1) - parseFloat(num2);
+    let resultmult = parseFloat(num1) * parseFloat(num2);
+    let resultdivide = parseFloat(num1) / parseFloat(num2);
+    let resulthtml = "<p>+ " + resultadd + "</p>" + "<p>- " + resultsub + "</p>" + "<p>* " + resultmult + "</p>" + "<p>/ " + resultdivide + "</p>";
+
+    document.getElementById("results").innerHTML = resulthtml;
 }
