1165ff190e0bbaced7727adf3979cc64f9cc17df
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-01-31 12:47:00 +0100

    useEffect

--- react/eigenesProjekt/meinprojekt/src/components/BTC.js
+++ react/eigenesProjekt/meinprojekt/src/components/BTC.js
@@ -5,12 +5,7 @@ class BTC extends Component {
         return (
             <div>
                 <ul>
-                    {fetch('https://api.coindesk.com/v1/bpi/currentprice.json')
-                        .then(response => response.json())
-                        .then(data => {
-                            return data.bpi.USD.rate;
-                        })}
-                    }
+                    <li></li>
                 </ul>
             </div>
         );
---
+++ react/eigenesProjekt/meinprojekt/src/components/todo.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+
+export default function Todo() {
+    const [todos, setTodos] = useState([]);
+    useEffect(() => {
+        return () => {
+            effect
+        };
+    }, [input]);
+    
+    return{
+        
+    }
+}
