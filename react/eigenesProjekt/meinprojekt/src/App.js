095bd3a396cf3961216c0f7f8a59476ea23c7100
Michael Mayer <39913724+dwaltsch@users.noreply.github.com>
2023-02-10 08:05:02 +0100

    stopf

--- react/eigenesProjekt/meinprojekt/src/App.js
+++ react/eigenesProjekt/meinprojekt/src/App.js
@@ -1,12 +1,11 @@
 import './App.css';
-import BTC from "./components/BTC";
+
 import ClosingButton from "./components/ClosingButton";
 function App() {
     return (
         <div className="App">
             <header className="App-header">
                 <ClosingButton/>
-                <BTC/>
             </header>
         </div>
     );
